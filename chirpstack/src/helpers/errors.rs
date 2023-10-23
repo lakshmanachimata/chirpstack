ea51acb9d7edea1a868ffe5eb1a4b03b06e0cbfb
Orne Brocaar <info@brocaar.com>
2023-10-23 13:04:01 +0100

    Update api error handling.

--- chirpstack/src/helpers/errors.rs
+++ chirpstack/src/helpers/errors.rs
@@ -13,3 +13,15 @@ impl PrintFullError for crate::storage::error::Error {
         format!("{:#}", self)
     }
 }
+
+impl PrintFullError for crate::api::auth::error::Error {
+    fn full(&self) -> String {
+        format!("{:#}", self)
+    }
+}
+
+impl PrintFullError for lrwn::Error {
+    fn full(&self) -> String {
+        format!("{:#}", self)
+    }
+}
